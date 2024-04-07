7fca69562fa5fe8a4b3fce0c617902a2a8e3673b
letiziabrunetti <letiziabrunetti.work@gmail.com>
2024-04-07 07:36:12 -0400

    lint AGAIN 10

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -11,7 +11,8 @@ class CarMake(models.Model):
 
 
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
+    car_make = models.ForeignKey(CarMake, 
+                                 on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
@@ -20,10 +21,10 @@ class CarModel(models.Model):
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
     year = models.PositiveIntegerField(default=2023,
-        validators=[
-            MaxValueValidator(2023),
-            MinValueValidator(2015)
-        ])
+                                       validators=[
+                                            MaxValueValidator(2023),
+                                            MinValueValidator(2015)
+                                       ])
 
     def __str__(self):
         return f'{self.name} - {self.type}'
