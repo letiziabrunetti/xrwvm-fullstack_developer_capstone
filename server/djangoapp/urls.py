2cf823739c81ca6e059ce6dfc2118d2ee5e44e24
letiziabrunetti <letiziabruneti.work@gmail.com>
2024-04-07 05:13:15 -0400

    incoming DB light

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -17,6 +17,4 @@ urlpatterns = [
     path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
     path(route='add_review', view=views.add_review, name='add_review'),
 
-    # path for add a review view
-
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,8 +28,8 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS=['localhost','https://letiziabrune-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/*']
-CSRF_TRUSTED_ORIGINS=['https://letiziabrune-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/*', 'https://letiziabrune-3030.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/*']
+ALLOWED_HOSTS=['localhost','https://letiziabrune-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/*']
+CSRF_TRUSTED_ORIGINS=['https://letiziabrune-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/*', 'https://letiziabrune-3030.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/*']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
